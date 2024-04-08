a9bf5586e0c49106e4c792269724e900a2509648
sammy <sammy@10.220.56.188>
2024-04-08 17:57:12 -0400

    DEalerFX

---
+++ Dealerfx/Dealer2.java
@@ -0,0 +1,59 @@
+package Dealerfx;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dealer2 extends Application {
+    private List<Integer> deck;
+    private int nextCardIndex;
+    private Label cardLabel;
+
+    @Override
+    public void start(Stage primaryStage) {
+        deck = new ArrayList<>();
+        resetDeck();
+
+        VBox root = new VBox();
+        cardLabel = new Label();
+
+        root.getChildren().add(cardLabel);
+
+        dealCardAndUpdateLabel();
+
+        Scene scene = new Scene(root, 200, 200);
+        primaryStage.setTitle("Dealer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void resetDeck() {
+        deck.clear();
+        for (int i = 1; i <= 10; i++) {
+            deck.add(i);
+        }
+        shuffleDeck();
+        nextCardIndex = 0;
+    }
+
+    private void shuffleDeck() {
+        Collections.shuffle(deck);
+    }
+
+    private void dealCardAndUpdateLabel() {
+        if (nextCardIndex >= deck.size()) {
+            resetDeck();
+        }
+        int card = deck.get(nextCardIndex++);
+        cardLabel.setText("Dealt card: " + card);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
